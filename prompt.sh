574ddb90c9922d6b46f183671987a31c0002ab01
lyiriyah <oscar.wittamsnangle@gmail.com>
2021-06-01 09:48:17 +0100

    prompt nicer

--- prompt.sh
+++ prompt.sh
@@ -1,9 +1,9 @@
-GIT_PS1_SHOWDIRTYSTATE=true
+GT_PS1_SHOWDIRTYSTATE=true
 GIT_PS1_SHOWSTASHSTATE=true
 GIT_PS1_SHOWUNTRACKEDFILES=true
 GIT_PS1_SHOWUPSTREAM="verbose"
 GIT_PS1_SHOWUNTRACKEDFILES=true
 GIT_PS1_SHOWCOLORHINTS=true
-. ~/.local/share/git-prompt.sh
+. ~/.git-prompt.sh
 
-PROMPT_COMMAND='__git_ps1 "$(tput setaf 23)\w$(tput sgr0) \q{my/vim-mode}" "\n$(tput setaf 12)\$$(tput sgr0) " " (%s) "'
+PROMPT_COMMAND='__git_ps1 "$(echo -e "\u256D\u2574")$(tput setaf 23)\u$(tput sgr0) in $(tput setaf 23)\w$(tput sgr0) \q{my/vim-mode}" "\n$(echo -e "\u2570\u2574")$(tput setaf 12)\$$(tput sgr0) " " (%s) "'
